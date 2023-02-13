620565cec216e20a9229e661993b551d3a297ac5
brightdaniel <ivquestservices@gmail.com>
2023-02-13 20:20:19 +0100

    loops

---
+++ week5/day1/mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int mul = 2;
+
+	for(int i = 1; i <= 12; i++)
+	{
+		printf("%d * %d = %d \n", mul, i, mul * i); 
+	}
+	
+
+
+
+
+}
---
+++ week5/day1/print.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main()
+{
+	int i;
+
+	for(; i < 10; i++)
+	{
+		printf("hello world\n");
+	}
+
+
+}
